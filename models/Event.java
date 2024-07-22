066de58b4cac5995d3c5ab0cba18e452a787f5a6
jesusreluz <jesusmanuelreluz@hotmail.com>
2024-07-22 14:52:14 -0500

    Create event model

---
+++ models/Event.java
@@ -0,0 +1,51 @@
+package models;
+
+public class Event {
+    private int date;
+    private int time;
+    private double latitude;
+    private double longitute;
+    private double depth;
+    private double magnitude;
+    private int cutoff_date;
+
+    // Constructor
+    public Event(int date, int time, double latitude, double longitute, double depth, double magnitude, int cutoff_date){
+        this.date = date;
+        this.time = time;
+        this.latitude = latitude;
+        this.longitute = longitute;
+        this.depth = depth;
+        this.magnitude = magnitude;
+        this.cutoff_date = cutoff_date;
+    }
+
+    // Getters
+    public int getDate(){
+        return date;
+    }
+
+    public int getTime(){
+        return time;
+    }
+
+    public double getLatitude(){
+        return latitude;
+    }
+
+    public double getLongitude(){
+        return longitute;
+    }
+
+    public double getDepth(){
+        return depth;
+    }
+
+    public double getMagnitude(){
+        return magnitude;
+    }
+
+    public int getCutoff_date(){
+        return cutoff_date;
+    }
+}
